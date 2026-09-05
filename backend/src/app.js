b3a0f1f173e42ebff8812f973bd9104e33422480
Aman Bhardwaj <aman08051978@gmail.com>
2026-09-05 16:43:50 +0530

    update app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,7 @@ import mongoose from "mongoose";
 import { ConnectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -19,6 +20,8 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 app.get("/home", (req,res) => {
     return res.json({"hello": "world"});
